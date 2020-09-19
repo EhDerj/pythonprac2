c769102d3a962c8b0e46c6d88dff8bd84c82ba59
Александров Алексей <s02180607@live.cs.msu.ru>
2020-09-19 19:04:27 +0300

    Add 3 and 4 problem

---
+++ 20200917_3/prob3.py
@@ -0,0 +1,9 @@
+i = 2
+while (i:= i+1) < 6:
+    j = 2
+    while (j:= j+1) < 6:
+        if j*i//10 + j*i%10 == 6:
+            print(':=)', end=' ')
+        else:
+            print(j*i, end=' ')
+    print()
