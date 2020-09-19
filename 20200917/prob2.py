91a0b66fe9d511b24b3ad967dd99d513aa815f23
Александров Алексей <s02180607@live.cs.msu.ru>
2020-09-19 17:58:13 +0300

    error

--- 20200917/prob2.py
+++
@@ -1,8 +0,0 @@
-b = 0
-while (a := int(input())) > 0 :
-    b += a
-    if (b>21) :
-        break
-else:
-    b = a;
-print(b)
--- 20200917/traing.py
+++
@@ -1,6 +0,0 @@
-a, b, c, = eval(input())
-
-if a>0 and b>0 and c>0 and a+b>c and b+c>a and c+a>b :
-    print("Possible")
-else:
-    print("Impossible")
