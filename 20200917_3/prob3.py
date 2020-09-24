e39fdbca8d4df2a2406ee3ac24cbbe9402bb2924
Александров Алексей <s02180607@live.cs.msu.ru>
2020-09-24 16:50:56 +0300

    fixed range

--- 20200917_3/prob3.py
+++ 20200917_3/prob3.py
@@ -1,7 +1,7 @@
 i = 2
-while (i:= i+1) < 6:
+while (i:= i+1) <= 6:
     j = 2
-    while (j:= j+1) < 6:
+    while (j:= j+1) <= 6:
         if j*i//10 + j*i%10 == 6:
             print(':=)', end=' ')
         else:
