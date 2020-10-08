b9ee1a673faca811c34becd77502d23a2b34bb76
Александров Алексей <s02180607@live.cs.msu.ru>
2020-10-08 17:43:17 +0300

    need to completegit status !

---
+++ 20201008_2/task2.py
@@ -0,0 +1,15 @@
+import fractions
+
+newf = lambda lst: fractions.Fraction(lst.pop(0))
+newi = lambda lst: int(lst.pop(0))
+
+def fnl(i, ls):
+    def f(x):
+        res = 0
+        while i >= 0
+
+def polinom_check(main):
+    main = main.split(', ')
+    s = newf(main)
+    w = newf(main)
+    for i in range(int(main.pop(0))):
