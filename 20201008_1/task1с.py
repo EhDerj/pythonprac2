ad3de6ed79926c97f9b0a39c02d1f3ed167fd148
ender <s02180607@stud.cs.msu.ru>
2020-10-14 19:52:07 +0300

    added first and second tasks

---
+++ 20201008_1/task1с.py
@@ -0,0 +1,42 @@
+mesh = [80, 25]
+seg_x = [-5, 5]
+seg_y = [-3, 3]
+
+def cell_2_point(x):
+    return seg_x[0] + (x + 0.5) * (seg_x[1] - seg_x[0]) / mesh[0]
+
+def point_2_cell(y):
+    return int((y - seg_y[0]) * mesh[1] / (seg_y[1] - seg_y[0]))
+
+def new_line(n):
+    s = ' ' * (n[0]-1) + '*'
+    for i in range(1, len(n)):
+        s += ' ' * (n[i]-n[i-1]-1) + '*'
+    return '{: <{}}'.format(s, mesh[0])
+
+def mesh_ver(f):
+    ls = list()
+    for x in range(mesh[0]):
+        y = point_2_cell(f(cell_2_point(x)))
+        for i in ls:
+            if i[0] == y:
+                i.append(x)
+                break
+        else:
+            if 0 < y < mesh[1]: ls.append([y, x])
+    ls.sort(key=lambda a: a[0], reverse=True)
+    j = 0
+    for i in reversed(range(mesh[1])):
+        if j < len(ls) and i == ls[j][0]:
+            print(new_line(ls[j][1:]))
+            j += 1
+        else:
+            print(' ' * mesh[0])
+
+
+from math import *
+
+s = input()
+if sg := input():
+    seg_x, seg_y = list(eval(sg)[:2]), list(eval(sg)[2:])
+mesh_ver(lambda x: eval(s))
--- 20201008_2/task2.py
+++ 20201008_2/task2.py
@@ -1,15 +1,30 @@
-import fractions
+from fractions import Fraction as frac
 
-newf = lambda lst: fractions.Fraction(lst.pop(0))
-newi = lambda lst: int(lst.pop(0))
+def str2rl(st):
+    st = st.split(', ')
+    for i in range(len(st)):
+        st[i] = float(frac(st[i]))
+    return st
 
-def fnl(i, ls):
+def str2fc(st):
+    st = st.split(', ')
+    for i in range(len(st)):
+        st[i] = frac(st[i])
+    return st
+
+def polynomizer(n, ls):
+    l = list()
+    for i in range(int(n) + 1):
+        l.append(ls.pop(0))
     def f(x):
         res = 0
-        while i >= 0
+        for i in range(int(n) + 1):
+            res += l[i] * x ** (n - i)
+        return res
+    return f
 
-def polinom_check(main):
-    main = main.split(', ')
-    s = newf(main)
-    w = newf(main)
-    for i in range(int(main.pop(0))):
+def polynom_check(main):
+    s, w = main.pop(0), main.pop(0)
+    A, B = polynomizer(main.pop(0), main), polynomizer(main.pop(0), main)
+    if B(s) == 0: return False
+    return A(s) / B(s) == w
