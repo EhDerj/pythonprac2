1e9fc8be166c49cc220a60659af08aaf66b6862d
ender <s02180607@stud.cs.msu.ru>
2020-11-12 13:36:23 +0300

    updated second task

--- 20201105_2/task2.py
+++ 20201105_2/task2.py
@@ -1,6 +1,5 @@
 from re import search, sub
-from sys import exit
 a = input()
-if not search('[aeiouy].+[aeiouy]', a):
-    exit()
-print(sub(r'([^aeiouy]+)([aeiouy])([^aeiouy]+)([aeiouy])(.+)', r'\1\4\3\2\5', a))
+if search('[aeiouy].+[aeiouy]', a):
+    a = sub(r'([^aeiouy]+)([aeiouy])([^aeiouy]+)([aeiouy])(.+)', r'\1\4\3\2\5', a)
+print(a)
